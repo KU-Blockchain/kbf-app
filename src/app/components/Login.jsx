5facd6638289341299d41852128df2309dc5958b
Micah Borghese <micahborghese@gmail.com>
2024-06-06 10:41:47 -0500

    cert auth

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -16,7 +16,7 @@ const Login = ({ isOpen, onClose }) => {
         if (isOpen) {
             const getJWT = async() => {
                 //const response = await fetch('../api/QuickLoginJWT'); moving to the kbf server to fetch JWT
-                const response = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER_IP}/api/QuickLoginJWT`);
+                const response = await fetch(`https://${process.env.NEXT_PUBLIC_SERVER_IP}/api/QuickLoginJWT`);
                 const data = await response.json();
                 setJwtChallenge(data.loginChallenge);
                 setJwtChallengeUrl(data.loginChallengeUrl);
@@ -40,7 +40,7 @@ const Login = ({ isOpen, onClose }) => {
                     //     },
                     //     body: JSON.stringify({ jwtChallenge: jwtChallenge }),
                     // });
-                    const response = await fetch(`http://${process.env.NEXT_PUBLIC_SERVER_IP}/api/QuickLoginSocket`, {
+                    const response = await fetch(`https://${process.env.NEXT_PUBLIC_SERVER_IP}/api/QuickLoginSocket`, {
                         method: 'POST',
                         headers: {
                             'Content-Type': 'application/json',
